e849545f2271d24859f37e0d642746bf355d7995
Sahil Soni <SscSPs@gmail.com>
2019-09-01 19:07:42 +0530

    Make a webview mobile app
    Signed-off-by: Sahil Soni <SscSPs@gmail.com>

---
+++ lib/resources/constants.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+int counter = 0;
+const AppPrimaryColor = primaryBlack;
+const AppTitle = 'ProteinXpress';
+const HomePageTitle = 'ProteinXpress';
+
+const MaterialColor primaryBlack = MaterialColor(
+  _blackPrimaryValue,
+  <int, Color>{
+    50: Color(0xFF000000),
+    100: Color(0xFF000000),
+    200: Color(0xFF000000),
+    300: Color(0xFF000000),
+    400: Color(0xFF000000),
+    500: Color(_blackPrimaryValue),
+    600: Color(0xFF000000),
+    700: Color(0xFF000000),
+    800: Color(0xFF000000),
+    900: Color(0xFF000000),
+  },
+);
+const int _blackPrimaryValue = 0xFF000000;
