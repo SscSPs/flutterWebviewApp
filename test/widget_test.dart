b700b6c18048863768a82c0fc36e38f028579d47
Sahil Soni <SscSPs@gmail.com>
2019-09-02 21:17:48 +0530

    Update the test
    Signed-off-by: Sahil Soni <SscSPs@gmail.com>

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 import '../lib/main.dart';
 
@@ -15,16 +16,8 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Just Verify that a WebView is loaded properly
+    expect(find.byType(WebviewScaffold), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
